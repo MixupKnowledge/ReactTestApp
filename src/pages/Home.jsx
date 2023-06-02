75fdfc1cbc10c5e5bf9cd604a9013db94d5b099d
Narendra Kumar <83486558+MixupKnowledge@users.noreply.github.com>
2023-06-02 10:55:39 +0530

    Create Home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Login from "../components/Login";
+import "../styles/Home.css"
+const Home = () => {
+  return (
+    <div className="mainContainer">
+      <h1>Welcome on Home Page </h1>
+      <Login />
+    </div>
+  );
+};
+
+export default Home;
